ffe4291e0b25f2f8ac7a2fb2661031c03a33e953
guthias <gmgustavom@gmail.com>
2021-11-23 17:38:44 -0300

    Removendo todos os elementos

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const taskList = document.getElementById('lista-tarefas');
 const taskText = document.getElementById('texto-tarefa');
 const addTaskButton = document.getElementById('criar-tarefa');
+const deleteAllBUtton = document.getElementById('apaga-tudo');
 
 function selectItem(event) {
   const elements = document.querySelectorAll('.selected');
@@ -32,4 +33,13 @@ function adicionarTarefa() {
   taskText.value = '';
 }
 
+function removeTodos() {
+  const elements = document.querySelectorAll('#lista-tarefas li');
+
+  for (let i = 0; i < elements.length; i += 1) {
+    elements[i].remove();
+  }
+}
+
 addTaskButton.addEventListener('click', adicionarTarefa);
+deleteAllBUtton.addEventListener('click', removeTodos);
